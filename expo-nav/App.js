4aea5c7444a0cd07a70ee761c538d8b375f08540
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-04-13 13:30:47 +0200

    access of navigators installation

--- expo-nav/App.js
+++ expo-nav/App.js
@@ -1,30 +1,40 @@
 import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { createDrawerNavigator } from "@react-navigation/drawer";
-// import First from "./components/First";
-// import Second from "./components/Second";
-import { Text , View} from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StatusBar } from "expo-status-bar";
+import First from "./components/First";
+import Second from "./components/Second";
 
-// const Stack = createNativeStackNavigator()
-// const Drawer = createDrawerNavigator()
+/*
+1. npm install react-native-reanimated@1 --save --save-exact
+2. npm install @react-navigation/native-stack
+3. npm install @react-navigation/drawer
+4. npx expo install react-native-gesture-handler react-native-reanimated
+*/
 
-// function DrawerNavigator() {
+const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
 
-//   return (
-//     <Drawer.Navigator>
-//       <Drawer.Screen name="First Name" component={First} />
-//       <Drawer.Screen name="Second Name" component={Second} />
-//     </Drawer.Navigator>
-//   )
-// }
+function DrawerNavigator() {
+
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="First Name" component={First} />
+      <Drawer.Screen name="Second Name" component={Second} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
 
   return (
-    <View>   
-        <Text>furehuifer</Text>
-
-    </View>
-    
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="DrawerScreen" component={DrawerNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
